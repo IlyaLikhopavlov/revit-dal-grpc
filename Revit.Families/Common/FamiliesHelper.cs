d5ebf36f9117d6aa8dce6ef835a964fd2ed6814c
Ilya Likhopavlov <pnr@comfortlab.ru>
2022-11-19 13:46:54 +0600

    added family rendering

---
+++ Revit.Families/Common/FamiliesHelper.cs
@@ -0,0 +1,71 @@
+﻿using Autodesk.Revit.DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Resources;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Revit.Families.Common
+{
+    public static class FamiliesHelper
+    {
+        public static FamilySymbol[] GetFamilySymbols(this Document document, string familyName)
+        {
+            using var collector = new FilteredElementCollector(document);
+            return collector
+                .OfClass(typeof(FamilySymbol))
+                .Select(n => (FamilySymbol)n)
+                .Where(n => n.FamilyName == familyName)
+                .ToArray();
+        }
+
+        public static FamilySymbol GetFamilySymbol(this Document document, string familyName, string symbolName)
+        {
+            using var collector = new FilteredElementCollector(document);
+            return collector
+                .OfClass(typeof(FamilySymbol))
+                .Select(n => (FamilySymbol)n)
+                .FirstOrDefault(n => n.FamilyName == familyName
+                                        && n.Name == symbolName);
+        }
+
+        public static bool IsFamilyExists(this Document document, string familyName)
+        {
+            using var collector = new FilteredElementCollector(document);
+            return collector
+                .OfClass(typeof(FamilySymbol))
+                .Select(n => (FamilySymbol)n)
+                .Any(n => n.FamilyName == familyName);
+        }
+
+        public static FamilyInstance[] GetFamilyInstances(this Document document, string familyName, ElementId elementId)
+        {
+            using var collector = new FilteredElementCollector(document, elementId);
+            return collector
+                .OfClass(typeof(FamilyInstance))
+                .Select(n => (FamilyInstance)n)
+                .Where(n => n.Symbol.FamilyName == familyName)
+                .ToArray();
+        }
+
+        public static List<Family> GetFamilies(this Document document)
+        {
+            using var collector = new FilteredElementCollector(document);
+            return collector
+                .OfClass(typeof(Family))
+                .Cast<Family>()
+                .ToList();
+        }
+
+        public static List<ReferencePlane> GetReferencePlanes(this Document document)
+        {
+            using var collector = new FilteredElementCollector(document);
+            return collector
+                .WhereElementIsNotElementType()
+                .OfClass(typeof(ReferencePlane))
+                .Select(n => (ReferencePlane)n)
+                .ToList();
+        }
+    }
+}
---
+++ Revit.Families/Rendering/Sample.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Resources;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Revit.DAL.Utils;
+using Revit.Families.Common;
+using Revit.Families.Rendering.Enums;
+
+namespace Revit.Families.Rendering
+{
+    public class Sample
+    {
+        private const string FamilyExtension = @".rfa";
+        
+        private static class FooNames
+        {
+            public const string Family = @"Revit.DAL.Foo";
+            public const string Resource = @"RevitDalFoo";
+        }
+
+        private static class BarNames
+        {
+            public const string Family = @"Revit.DAL.Bar";
+            public const string Resource = @"RevitDalBar";
+        }
+
+        private readonly Document _document;
+        private readonly Application _application;
+
+        public Sample(UIApplication uiApplication)
+        {
+            _application = uiApplication.Application;
+            _document = uiApplication.ActiveUIDocument.Document;
+        }
+
+        public FamilySymbol Render(FamilyTypeEnum familyType)
+        {
+            var name = familyType switch
+            {
+                FamilyTypeEnum.Foo => (FooNames.Family, FooNames.Resource),
+                FamilyTypeEnum.Bar => (BarNames.Family, BarNames.Resource),
+                _ => throw new ArgumentOutOfRangeException(nameof(familyType), familyType, null)
+            };
+
+            var sampleSymbol = _document.GetFamilySymbols(name.Family).FirstOrDefault();
+            if (sampleSymbol != null)
+            {
+                return sampleSymbol;
+            }
+
+            var familyData =  Families.ResourceManager.GetObject(name.Resource) as byte[];
+            var tempFileName = $"{Path.GetTempFileName()}{FamilyExtension}";
+            File.WriteAllBytes(tempFileName, familyData!);
+
+            _document.ExecuteTransaction(() =>
+            {
+                _document.LoadFamily(tempFileName, new FamilyLoadOptions(), out var family);
+                family.Name = name.Family;
+            }, "Rename family");
+            File.Delete(tempFileName);
+
+
+            return _document.GetFamilySymbols(name.Family).First();
+        }
+    }
+}
